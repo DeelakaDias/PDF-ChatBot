2dd9058f42cec9c4940092fbd1af12095cf2ebf0
Deelaka Dias <deelakadias2002@gmail.com>
2024-08-12 09:24:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDF here and click on 'Process'")
-        st.button("Process")
+        pdf_docs = st.file_uploader(
+                "Upload your PDF here and click on 'Process'", accept_multiple_files = True  )
+        if st.button("Process"):
+            with st.spinner("Processing"):
+            #  get pdf text
+            raw_text = get_pdf_text(pdf_docs)
+            # get the text chucks 
+
+            # create vector store
 
 if __name__ == '__main__':
     main()
